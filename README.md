e5aafdcf5e43adb1bbf2fa1ac6e8cc4a15d43cda
Eduardo Savian <eduardo.savian@trust.group>
2025-02-06 09:03:11 -0300

    Change code structure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Knight's tour algorithms
+# Knight's Tour Algorithms
 
-Madde by Eduardo Savian, Mateus Winter
+Made by Eduardo Savian, Mateus Winter
 
 ## Description
 
@@ -10,24 +10,55 @@ Implement two algorithms for the Knight's Walk:
 
 - Another should be a strategy that minimizes computational performance. Do not use ready-made algorithms such as Warnsdorff. The suggested strategy is to choose the smallest possible move.
 
-## Run
+## How to Run
 
-### Linux
+### Prerequisites
 
-#### Go build
+- Ensure you have [Go](https://golang.org/dl/) installed.
+
+### Build
+
+#### Linux
+
+1. Open your terminal.
+
+2. Navigate to the project directory where the source files are located.
+
+3. Run the following command to build the executable:
 
 ```bash
-go build -o knight_tour src/main.go src/methods.go
+go build -o knights_tour src/main.go src/methods.go
 ```
 
-#### Run executable
+### Execute
+
+1. Once the build is complete, you can run the executable.
+
+2. Use the following command to run the program:
 
 ```bash
-./knights_tour
+./knights_tour <boardSize> <startX> <startY> <algorithm>
 ```
 
-## Bibliographic references
+- **boardSize**: Size of the chessboard (e.g., 8 for an 8x8 board).
+- **startX**: Starting X-coordinate of the knight.
+- **startY**: Starting Y-coordinate of the knight.
+- **algorithm**: The algorithm to use for the Knight’s tour. Options are:
+- `wd` (Warnsdorff)
+- `bt` (Backtrack)
+- `hd` (High Degree)
+- `sf` (Shuffle)
 
-[An efficient algorithm for the Knight’s tour problem - Ian Parberry](https://core.ac.uk/download/pdf/81964499.pdf)
+#### Example Command
 
-[Knight's tour - Wikipedia](https://en.wikipedia.org/wiki/Knight's_tour)
+```bash
+./knights_tour 8 0 0 wd
+```
+
+This command runs the program with an 8x8 board, starting at position (0,0), using the Warnsdorff algorithm.
+
+## Bibliographic References
+
+- [An efficient algorithm for the Knight’s tour problem - Ian Parberry](https://core.ac.uk/download/pdf/81964499.pdf)
+
+- [Knight's tour - Wikipedia](https://en.wikipedia.org/wiki/Knight's_tour)
--- src/main.go
+++ src/main.go
@@ -1,55 +1,65 @@
 package main
 
 import (
-    "context"
     "fmt"
     "os"
     "strconv"
     "time"
 )
 
-func printBoard(board [][]int) {
-	fmt.Println("          Board")
+func printBoard(board [][]int, print_time ...time.Duration) {
+    fmt.Println("Board:")
+
     for _, row := range board {
         for _, cell := range row {
-            fmt.Printf("%2d ", cell)
+            fmt.Printf("%02d ", cell)
         }
         fmt.Println()
     }
-	fmt.Println()
+    fmt.Println()
+
+    if len(print_time) > 0 && print_time[0] > 0 {
+        fmt.Println("Time: ", print_time[0])
+    }
 }
 
-func main() {
+func handleMenu() (int, int, int, string) {
     args := os.Args
 
     if len(args) < 5 {
         fmt.Println("Usage: <boardSize> <startX> <startY> <algorithm>")
-        return
+        os.Exit(1)
     }
 
     boardSize, err := strconv.Atoi(args[1])
     if err != nil || boardSize <= 0 {
         fmt.Println("Invalid board size. Must be a positive integer.")
-        return
+        os.Exit(1)
     }
 
     startX, err := strconv.Atoi(args[2])
     if err != nil || startX < 0 || startX >= boardSize {
         fmt.Println("Invalid startX. Must be within the board size.")
-        return
+        os.Exit(1)
     }
 
     startY, err := strconv.Atoi(args[3])
     if err != nil || startY < 0 || startY >= boardSize {
         fmt.Println("Invalid startY. Must be within the board size.")
-        return
+        os.Exit(1)
     }
 
     algorithm := args[4]
     if algorithm != "wd" && algorithm != "bt" && algorithm != "hd" && algorithm != "sf" {
         fmt.Println("Invalid algorithm. Must be either 'wd' (warnsdorff), 'bt' (backtrack), 'hd' (highDegree), or 'sf' (shuffle).")
-        return
-	}
+        os.Exit(1)
+    }
+
+    return boardSize, startX, startY, algorithm
+}
+
+func main() {
+    boardSize, startX, startY, algorithm := handleMenu()
 
     board := make([][]int, boardSize)
     for i := range board {
@@ -61,37 +71,22 @@ func main() {
     time_start := time.Now()
 
     if algorithm == "wd" {
-		printBoard(board)
+        printBoard(board)
         solution = greedySearch(board, startX, startY, boardSize, algorithm)
     }
 
     if algorithm == "bt" || algorithm == "hd" || algorithm == "sf" {
-        resultChan := make(chan bool)
-        ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-        defer cancel()
-
-        go func() {
-            resultChan <- backtrackSearch(board, 1, startX, startY, boardSize, algorithm)
-        }()
-
-        select {
-        case <-ctx.Done():
-            solution = false
-        case result := <-resultChan:
-            solution = result
-        }
+        solution = backtrackSearch(board, 1, startX, startY, boardSize, algorithm)
     }
 
     time_end := time.Now()
 
     time_result := time_end.Sub(time_start)
 
-    fmt.Println("Time: ", time_result)
-
     if !solution {
-        fmt.Println("No Knight's tour solution found:", err)
+        fmt.Println("No Knight's tour solution found.")
         return
     }
 
-    printBoard(board)
+    printBoard(board, time_result)
 }
