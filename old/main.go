223a5d2f39e430bd09daf0ef379c0a8ca5c3ca67
eduardosavian <eduardosavian@outlook.com>
2025-03-04 20:42:52 -0300

    Add old version

---
+++ old/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type knight struct {
+    Algorithm   string  `json:"algorithm"`
+    Size        int     `json:"size"`
+    StartX      int     `json:"startX"`
+    StartY      int     `json:"startY"`
+}
+
+
+var testss = []knight {
+    {"wd", 8, 0, 0},
+}
+
+var count = 0
+
+func main() {
+    // boardSize, startX, startY, algorithm := handleMenu()
+
+    // board := make([][]int, boardSize)
+    // for i := range board {
+    //     board[i] = make([]int, boardSize)
+    // }
+
+    // time_start  := time.Now()
+    // solution    := backtrackSearch(board, 1, startX, startY, boardSize, algorithm)
+    // time_result := time.Since(time_start)
+
+    // options     := PrintOptions{
+    //     Time:    time_result ,
+    //     Solution: solution,
+    //     Count: count,
+    // }
+
+    // printBoard(board, options)
+
+    router := gin.Default()
+    router.GET("/knights_tour", getKnightsTour)
+    router.Run("localhost:8585")
+}
+
+func getKnightsTour(c * gin.Context) {
+    c.IndentedJSON(http.StatusOK, testss)
+}
